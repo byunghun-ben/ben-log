ed2ed2bedea397074d53b3e9b920396aab453302
byunghun-ben <figma@kakao.com>
2024-10-13 23:05:01 +0900

    vite 설정 차이에 대한 주석 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { defineConfig } from "vite";
+// TODO: vite는 plugin-react-swc를 사용하고 있음.
+import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite({}), react()],
-})
+});
